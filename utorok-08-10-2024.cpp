ac5a54ce631ed7468b4c25176947b6ab0a485c22
velkayolanda <tomo.simurda@gmail.com>
2024-11-19 16:39:01 +0100

    Update utorok-08-10-2024.cpp

--- utorok-08-10-2024.cpp
+++ utorok-08-10-2024.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include <cmath>
+#include <time.h>
+#include <ctime>
+#include <iomanip>
+#include <vector>
+#include <chrono>
+#include <cfloat>
+#define PI 3.141592653 
+
+using namespace std;
 
